93f17490e20bb915078909d9fe2ca5ed819d87f7
Diego Camargo <dicamargov@gmail.com>
2016-05-11 17:24:12 -0500

    added more datasets

--- dt.py
+++ dt.py
@@ -1,9 +1,9 @@
 from DecisionTree import DecisionTree
 
 dt = DecisionTree(
-  training_datafile = "data/stage3cancer.csv",
+  training_datafile = "data/dataset.csv",
   csv_class_column_index = 2,
-  csv_columns_for_features = [3,4,5,6,7,8],
+  csv_columns_for_features = [1,2,3,4,5],
   entropy_threshold = 0.01,
   max_depth_desired = 8,
   symbolic_to_numeric_cardinality_threshold = 10,
@@ -16,11 +16,10 @@ dt.show_training_data()
 root_node = dt.construct_decision_tree_classifier()
 root_node.display_decision_tree("   ")
 
-test_sample  = ['g2 = 4.2',
-                'grade = 2.3',
-                'gleason = 4',
-                'eet = 1.7',
-                'age = 55.0',
-                'ploidy = diploid']
+test_sample  = ['ColorDeCabello = Negro',
+                'Altura = Alto',
+                'Peso = Alto',
+                'Proteccion = No',
+                'Quemadura = Si']
 classification = dt.classify(root_node, test_sample)
 print "Classification: ", classification
